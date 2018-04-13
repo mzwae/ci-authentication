0d243638f3f009c4e5788ef0742e19fd02a04220
mzwae <mrmonjaie@gmail.com>
2018-04-13 03:24:40 +0100

    updated users model

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -55,12 +55,71 @@ class Users_model extends CI_Model
         }
     }
 
-    public function delete_user($id){
-      if ($this->db->delete('users', array('usr_id' => $id))) {
-        return true;
-      } else {
-        return false;
-      }
+    public function delete_user($id)
+    {
+        if ($this->db->delete('users', array('usr_id' => $id))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    public function make_code()
+    {
+        do {
+            $url_code = random_string('alnum', 8);
+
+            $this->db->where('usr_pwd_change_code = ', $url_code);
+            $this->db->from('users');
+            $num = $this->db->count_all_results();
+        } while ($num >= 1);
+
+        return $url_code;
+    }
+
+    public function count_results($email)
+    {
+        $this->db->where('usr_email', $email);
+        $this->db->from('users');
+        return $this->db->count_all_results();
+    }
+
+    public function update_user_password($data)
+    {
+        $this->db->where('usr_id', $data['usr_id']);
+        if ($this->db->update('users', $data)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function does_code_match($code, $email)
+    {
+        $query = "SELECT COUNT(*) AS count
+                FROM users
+                WHERE usr_pwd_change_code = ?
+                AND usr_email = ?";
+
+        $res = $this->db->query($query, array($code, $email));
+        foreach ($res->result() as $row) {
+            $count = $row->count;
+        }
+
+        if ($count == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function update_user_code($data)
+    {
+        $this->db->where('usr_email', $data['usr_email']);
+        if ($this->db->update('users', $data)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
