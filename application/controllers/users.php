468e6a442a533b17a8aaf4ec5bc84dd776d4903e
mzwae <mrmonjaie@gmail.com>
2018-04-28 12:13:21 +0100

    added delete user

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -31,7 +31,7 @@ class Users extends MY_Controller
         $this->form_validation->set_rules('usr_lname', 'Last Name', 'required');
         $this->form_validation->set_rules('usr_uname', 'Username', 'required');
         $this->form_validation->set_rules('usr_email', 'Email', 'required|valid_email|is_unique[users.usr_email]');
-        $this->form_validation->set_rules('usr_confirm_email', 'Confirm Email','required|valid_email|matches[usr_email]');
+        $this->form_validation->set_rules('usr_confirm_email', 'Confirm Email', 'required|valid_email|matches[usr_email]');
         $this->form_validation->set_rules('usr_add1', 'Address Line 1', 'required');
         $this->form_validation->set_rules('usr_add2', 'Address Line 2', 'required');
         $this->form_validation->set_rules('usr_add3', 'Address Line 3', 'required');
@@ -77,7 +77,7 @@ class Users extends MY_Controller
                               'name' => 'usr_confirm_email',
                               'class' => 'form-control',
                               'id' => 'usr_confirm_email',
-                              'value' => set_value('usr_confirm_email',''),
+                              'value' => set_value('usr_confirm_email', ''),
                               'maxlength' => '100',
                               'size' => '35'
                             );
@@ -126,10 +126,10 @@ class Users extends MY_Controller
             $this->load->view('users/new_user', $data);
             $this->load->view('templates/footer', $data);
         } else { // Validation passed
-          $password = random_string('alnum', 8);
-          $hash = $password;
+            $password = random_string('alnum', 8);
+            $hash = $password;
 
-          $data = array(
+            $data = array(
                   'usr_fname' => $this->input->post('usr_fname'),
                   'usr_lname' => $this->input->post('usr_lname'),
                   'usr_uname' => $this->input->post('usr_uname'),
@@ -144,64 +144,62 @@ class Users extends MY_Controller
                   'usr_is_active' => $this->input->post('usr_is_active')
                 );
 
-          if ($this->Users_model->process_create_user($data)) {
-            $file = read_file('../views/email_scripts/welcome.txt');
-            $file = str_replace('%usr_fname%', $data['usr_fname'], $file);
-            $file = str_replace('%usr_lname%', $data['usr_lname'], $file);
-            $file = str_replace('%password%', $passowrd, $file);
-            redirect('users');
-          } else {
-            # code...
-          }
-
-
-
+            if ($this->Users_model->process_create_user($data)) {
+                $file = read_file('../views/email_scripts/welcome.txt');
+                $file = str_replace('%usr_fname%', $data['usr_fname'], $file);
+                $file = str_replace('%usr_lname%', $data['usr_lname'], $file);
+                $file = str_replace('%password%', $passowrd, $file);
+                redirect('users');
+            } else {
+                # code...
+            }
         }
     }
 
-    public function edit_user(){
-      // Set validation rules
-      $this->form_validation->set_rules('usr_id', 'User ID', 'required');
-      $this->form_validation->set_rules('usr_fname', 'First Name', 'required');
-      $this->form_validation->set_rules('usr_lname', 'Last Name', 'required');
-      $this->form_validation->set_rules('usr_uname', 'Username', 'required');
-      $this->form_validation->set_rules('usr_email', 'Email', 'required|valid_email|is_unique[users.usr_email]');
-      $this->form_validation->set_rules('usr_confirm_email', 'Confirm Email','required|valid_email|matches[usr_email]');
-      $this->form_validation->set_rules('usr_add1', 'Address Line 1', 'required');
-      $this->form_validation->set_rules('usr_add2', 'Address Line 2', 'required');
-      $this->form_validation->set_rules('usr_add3', 'Address Line 3', 'required');
-      $this->form_validation->set_rules('usr_town_city', 'Town/City', 'required');
-      $this->form_validation->set_rules('usr_zip_pcode', 'Post Code', 'required');
-      $this->form_validation->set_rules('usr_access_level', 'User Access Level', 'min_length[1]|max_length[125]');
-      $this->form_validation->set_rules('usr_is_active', 'User is active?', 'required');
+    public function edit_user()
+    {
+        // Set validation rules
+        $this->form_validation->set_rules('usr_id', 'User ID', 'required');
+        $this->form_validation->set_rules('usr_fname', 'First Name', 'required');
+        $this->form_validation->set_rules('usr_lname', 'Last Name', 'required');
+        $this->form_validation->set_rules('usr_uname', 'Username', 'required');
+        $this->form_validation->set_rules('usr_email', 'Email', 'required|valid_email|is_unique[users.usr_email]');
+        $this->form_validation->set_rules('usr_confirm_email', 'Confirm Email', 'required|valid_email|matches[usr_email]');
+        $this->form_validation->set_rules('usr_add1', 'Address Line 1', 'required');
+        $this->form_validation->set_rules('usr_add2', 'Address Line 2', 'required');
+        $this->form_validation->set_rules('usr_add3', 'Address Line 3', 'required');
+        $this->form_validation->set_rules('usr_town_city', 'Town/City', 'required');
+        $this->form_validation->set_rules('usr_zip_pcode', 'Post Code', 'required');
+        $this->form_validation->set_rules('usr_access_level', 'User Access Level', 'min_length[1]|max_length[125]');
+        $this->form_validation->set_rules('usr_is_active', 'User is active?', 'required');
 
-      if ($this->input->post()) {
-        $id = $this->input->post('usr_id');
-      } else {
-        $id = $this->uri->segment(3);
-      }
+        if ($this->input->post()) {
+            $id = $this->input->post('usr_id');
+        } else {
+            $id = $this->uri->segment(3);
+        }
 
-      $data['page_heading'] = 'Edit user';
+        $data['page_heading'] = 'Edit user';
 
-      // Begin Validation
-      if ($this->form_validation->run() == FALSE) { // First load, or problem with form
+        // Begin Validation
+      if ($this->form_validation->run() == false) { // First load, or problem with form
             $query = $this->Users_model->get_user_details($id);
-            foreach ($query->result() as $row) {
-            $usr_id = $row->usr_id;
-            $usr_fname = $row->usr_fname;
-            $usr_lname = $row->usr_lname;
-            $usr_uname = $row->usr_uname;
-            $usr_email = $row->usr_email;
-            $usr_add1 = $row->usr_add1;
-            $usr_add2 = $row->usr_add2;
-            $usr_add3 = $row->usr_add3;
-            $usr_town_city = $row->usr_town_city;
-            $usr_zip_pcode = $row->usr_zip_pcode;
-            $usr_access_level = $row->usr_access_level;
-            $usr_is_active = $row->usr_is_active;
-            }
+          foreach ($query->result() as $row) {
+              $usr_id = $row->usr_id;
+              $usr_fname = $row->usr_fname;
+              $usr_lname = $row->usr_lname;
+              $usr_uname = $row->usr_uname;
+              $usr_email = $row->usr_email;
+              $usr_add1 = $row->usr_add1;
+              $usr_add2 = $row->usr_add2;
+              $usr_add3 = $row->usr_add3;
+              $usr_town_city = $row->usr_town_city;
+              $usr_zip_pcode = $row->usr_zip_pcode;
+              $usr_access_level = $row->usr_access_level;
+              $usr_is_active = $row->usr_is_active;
+          }
 
-            $data['usr_fname'] = array(
+          $data['usr_fname'] = array(
                                   'name' => 'usr_fname',
                                   'class' => 'form-control',
                                   'id' => 'usr_fname',
@@ -209,7 +207,7 @@ class Users extends MY_Controller
                                   'maxlength' => '100',
                                   'size' => '35'
                                   );
-            $data['usr_lname'] = array(
+          $data['usr_lname'] = array(
                                   'name' => 'usr_lname',
                                   'class' => 'form-control',
                                   'id' => 'usr_lname',
@@ -217,7 +215,7 @@ class Users extends MY_Controller
                                   'maxlength' => '100',
                                   'size' => '35'
                                   );
-            $data['usr_uname'] = array(
+          $data['usr_uname'] = array(
                                 'name' => 'usr_uname',
                                 'class' => 'form-control',
                                 'id' => 'usr_uname',
@@ -225,22 +223,22 @@ class Users extends MY_Controller
                                 'maxlength' => '100',
                                 'size' => '35'
                               );
-            $data['usr_email'] = array('name' => 'usr_email',
+          $data['usr_email'] = array('name' => 'usr_email',
                                'class' => 'form-control',
                                'id' => 'usr_email',
                                'value' => set_value('usr_email', ''),
                                'maxlength' => '100',
                                'size' => '35'
                              );
-            $data['usr_confirm_email'] = array(
+          $data['usr_confirm_email'] = array(
                               'name' => 'usr_confirm_email',
                               'class' => 'form-control',
                               'id' => 'usr_confirm_email',
-                              'value' => set_value('usr_confirm_email',''),
+                              'value' => set_value('usr_confirm_email', ''),
                               'maxlength' => '100',
                               'size' => '35'
                             );
-            $data['usr_add1'] = array(
+          $data['usr_add1'] = array(
                               'name' => 'usr_add1',
                               'class' => 'form-control',
                               'id' => 'usr_add1',
@@ -248,7 +246,7 @@ class Users extends MY_Controller
                               'maxlength' => '100',
                               'size' => '35'
                             );
-            $data['usr_add2'] = array(
+          $data['usr_add2'] = array(
                               'name' => 'usr_add2',
                               'class' => 'form-control',
                               'id' => 'usr_add2',
@@ -256,7 +254,7 @@ class Users extends MY_Controller
                               'maxlength' => '100',
                               'size' => '35'
                             );
-            $data['usr_add3'] = array(
+          $data['usr_add3'] = array(
                               'name' => 'usr_add3',
                               'class' => 'form-control',
                               'id' => 'usr_add3',
@@ -264,7 +262,7 @@ class Users extends MY_Controller
                               'maxlength' => '100',
                               'size' => '35'
                             );
-            $data['usr_town_city'] = array(
+          $data['usr_town_city'] = array(
                               'name' => 'usr_town_city',
                               'class' => 'form-control',
                               'id' => 'usr_town_city',
@@ -272,7 +270,7 @@ class Users extends MY_Controller
                               'maxlength' => '100',
                               'size' => '35'
                             );
-            $data['usr_zip_pcode'] = array(
+          $data['usr_zip_pcode'] = array(
                               'name' => 'usr_zip_pcode',
                               'class' => 'form-control',
                               'id' => 'usr_zip_pcode',
@@ -280,14 +278,63 @@ class Users extends MY_Controller
                               'maxlength' => '100',
                               'size' => '35'
                             );
-            $data['usr_access_level_options'] = array(1=>1, 2=>2, 3=>3, 4=>4, 5=>5);
-            $data['usr_access_level'] = array('value' => set_value('usr_access_level', ''));
-            $data['usr_is_active'] = $usr_is_active;
-            $data['id'] = array('usr_id' => set_value('usr_id', $usr_id));
-            $this->load->view('templates/header', $data);
-            $this->load->view('users/new_user', $data);
-            $this->load->view('templates/footer', $data);
+          $data['usr_access_level_options'] = array(1=>1, 2=>2, 3=>3, 4=>4, 5=>5);
+          $data['usr_access_level'] = array('value' => set_value('usr_access_level', ''));
+          $data['usr_is_active'] = $usr_is_active;
+          $data['id'] = array('usr_id' => set_value('usr_id', $usr_id));
+          $this->load->view('templates/header', $data);
+          $this->load->view('users/edit_user', $data);
+          $this->load->view('templates/footer', $data);
+      } else { //Validation passed
+          $data = array(
+                  'usr_fname' => $this->input->post('usr_fname'),
+                  'usr_lname' => $this->input->post('usr_lname'),
+                  'usr_una
 
+                  me' => $this->input->post('usr_uname'),
+                  'usr_email' => $this->input->post('usr_email'),
+                  'usr_add1' => $this->input->post('usr_add1'),
+                  'usr_add2' => $this->input->post('usr_add2'),
+                  'usr_add3' => $this->input->post('usr_add3'),
+                  'usr_town_city' => $this->input->post('usr_town_city'),
+                  'usr_zip_pcode' => $this->input->post('usr_zip_pcode'),
+                  'usr_access_level' => $this->input->post('usr_access_level'),
+                  'usr_is_active' => $this->input->post('usr_is_active')
+                  );
+
+          if ($this->Users_model->process_update_user($id, $data)) {
+              redirect('users');
+          }
+      }
     }
 
+    public function delete_user()
+    {
+        // Set validation rules
+        $this->form_validation->set_rules('id', 'User ID', 'required|integer');
+        if ($this->input->post()) {
+            $id = $this->input->post('id');
+        } else {
+            $id = $this->uri->segment(3);
+        }
+
+        $data['page_heading'] = 'Confirm delete?';
+        if ($this->form_validation->run() == false) {
+            $data['query'] = $this->Users_model->get_user_details($id);
+            $this->load->view('templates/header', $data);
+            $this->load->view('users/delete_user', $data);
+            $this->load->view('templates/footer', $data);
+        } else {
+            if ($this->Users_model->delete_user($id)) {
+                redirect('users');
+            }
+        }
+    }
+
+    public function pwd_email()
+    {
+        $id = $this->uri->segment(3);
+        send_email($data, 'reset');
+        redirect('users');
+    }
 }
